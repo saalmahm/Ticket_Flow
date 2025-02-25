c6877066829da5c170fc54f6c8346a68a211c563
salma hamdi <saalmahm51@gmail.com>
2025-02-25 21:57:48 +0000

    Crée les routes en utilisant prefix pour les différents views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::prefix('client')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('client.dashboard');
+    });
+});
+
+Route::prefix('developer')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('developer.dashboard');
+    });
+});
